3eb78a97c3bce4f21c047180d9d0d3b49b653aae
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2021-01-26 18:08:35 -0600

    added sql file

---
+++ FormulaOneDB.sql
@@ -0,0 +1,85 @@
+-- Create all 8 tables for Excellent_Simpson_Analysis db
+
+-- Drop tables using CASCADE to remove dependencies as well
+DROP TABLE IF EXISTS circuits CASCADE;
+
+DROP TABLE IF EXISTS races CASCADE;
+
+DROP TABLE IF EXISTS constructors CASCADE;
+
+DROP TABLE IF EXISTS constructor_results CASCADE;
+
+DROP TABLE IF EXISTS constructor_standings CASCADE;
+
+DROP TABLE IF EXISTS results CASCADE;
+
+DROP TABLE IF EXISTS drivers CASCADE;
+
+DROP TABLE IF EXISTS lap_times CASCADE;
+
+DROP TABLE IF EXISTS qualifying CASCADE;
+
+CREATE TABLE Season_Year (
+	Season_Number INT NOT NULL UNIQUE,
+	Season_Year_Aired INT NOT NULL UNIQUE, 
+ 	PRIMARY KEY(Season_Number)
+);
+
+-- Did not get clean data for all episodes and titles
+CREATE TABLE All_Episodes (
+	Season_Number INT NOT NULL,
+	Episode_Number INTEGER NOT NULL,
+	Episode_Title VARCHAR(50),
+ 	PRIMARY KEY(Season_Number, Episode_Number),
+	FOREIGN KEY(Season_Number) REFERENCES Season_Year(Season_Number)
+);
+
+CREATE TABLE Baby_Names_Popularity (
+	Season_Year_Aired INT NOT NULL,
+	Name_Rank INTEGER NOT NULL,
+	Character_Name VARCHAR(150) NOT NULL,
+	PRIMARY KEY(Season_Year_Aired, Character_Name),
+	FOREIGN KEY(Season_Year_Aired) REFERENCES Season_Year(Season_Year_Aired)
+);
+
+CREATE TABLE Guest_Stars_All (
+	Season_Number INT NOT NULL,
+	Guest_Star_Name VARCHAR(150) NOT NULL,
+	Episode_title VARCHAR(150) NOT NULL,
+	PRIMARY KEY(Guest_Star_Name, Episode_title),
+	FOREIGN KEY(Season_Number) REFERENCES Season_Year(Season_Number)
+);
+
+CREATE TABLE Guest_Star_Season (
+	Season_Number INT NOT NULL,
+	Count_of_Appearances INT NOT NULL,
+	PRIMARY KEY(Season_Number),
+	FOREIGN KEY(Season_Number) REFERENCES Season_Year(Season_Number)
+);
+
+CREATE TABLE Viewers (
+	Season_Number INT NOT NULL,
+	Number_of_Episodes INT NOT NULL,
+	Average_Ep_Viewers_in_mil FLOAT NOT NULL,
+	Most_Watched_Ep_Viewer_in_mil FLOAT NOT NULL,
+	Episode_Title VARCHAR(150),
+	PRIMARY KEY(Season_Number),
+	FOREIGN KEY(Season_Number) REFERENCES Season_Year(Season_Number)
+);
+
+CREATE TABLE Ratings (
+	Season_Number INT NOT NULL,
+	Number_of_Episodes INT NOT NULL,
+	Season_Rank INT NOT NULL,
+	Rating FLOAT NOT NULL,
+	PRIMARY KEY(Season_Number),
+	FOREIGN KEY(Season_Number) REFERENCES Season_Year(Season_Number)
+);
+
+CREATE TABLE Episode_Phrases (
+	Season_Number INT NOT NULL,
+	Episode_Number INT NOT NULL,
+	Most_common_phrase VARCHAR(150) NOT NULL,
+	Occurences INT NOT NULL,
+	FOREIGN KEY(Season_Number) REFERENCES Season_Year(Season_Number)
+);
