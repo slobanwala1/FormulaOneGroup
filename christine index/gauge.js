468a3873bcf2ab6707a5287bfe444f038de82d66
Christine Magnotta <christinemagnotta@CHRISTINEs-MacBook-Air.local>
2021-01-26 23:44:13 -0600

    Changed the html files.

--- christine index/gauge.js
+++ christine index/gauge.js
@@ -1,9 +1,8 @@
 //fastestLapSpeed
-
 var chart = c3.generate({
     data: {
         columns: [
-            ['fastestLapSpeed', 230]
+            ['fastestLapSpeed', 201] //so i'm going to add the data here from the sql data.
         ],
         type: 'gauge',
         onclick: function (d, i) { console.log("onclick", d, i); },
@@ -17,13 +16,13 @@ var chart = c3.generate({
             },
             show: false // to turn off the min/max labels.
         },
-    min: 200, 
-    max: 225, 
+    min: 200, //I'M GOING TO CHANGE THIS SO THAT THE MIN WILL BE THE SLOWEST FAST LAP
+    max: 225, //I'M GOING TO CHANGE THIS SO THAT THIS IS THE FASTEST FAST LAP
     units: ' %',
     width: 39 // for adjusting arc thickness
     },
     color: {
-        pattern: ['#f0f5d5', '#f55916', '#f76628', '#f20525'],
+        pattern: ['#F0F5D5', '#F55916', '#F76628', '#F20525'],
         threshold: {
             unit: 'value', // percentage is default
             max: 225,
@@ -34,58 +33,58 @@ var chart = c3.generate({
         height: 180
     }
 });
-
 var ticksValues = [200, 205, 210, 215, 217, 219, 221, 223];
-
-var ticks = d3.select('.c3-chart-arcs')
-        .append('g')
-      .selectAll("line")
-        .data(ticksValues).enter()
-      .append("line")
-        .attr("x1", 0)
-        .attr("x2", 0)
-        .attr("y1", -chart.internal.radius+8)
-        .attr("y2", -chart.internal.radius)
-        .attr("transform", function (d) {
-            var min = chart.internal.config.gauge_min,
-                max = chart.internal.config.gauge_max,
-                ratio = d / (max - min),
-                rotate = (d - 2) * 45;
-            return "rotate(" + rotate + ")";
-        });
-
-setTimeout(function () {
-    chart.load({
-        columns: [['data', 201]]
-    });
-}, 500);
-
-setTimeout(function () {
-    chart.load({
-        columns: [['data', 205]]
-    });
-}, 1000);
-
-setTimeout(function () {
-    chart.load({
-        columns: [['data', 210]]
-    });
-}, 1500);
-
-setTimeout(function () {
-    chart.load({
-        columns: [['data', 215]]
-    });
-}, 2000);
-
-setTimeout(function () {
-    chart.load({
-        columns: [['data', 220]]
-    });
-}, 2500);
-
-setTimeout(function () {
-    chart.load({
-        columns: [['data']]
-    });
+var test = true;
+setInterval(function(){
+  var ticks = d3.select('.c3-chart-arcs')
+          .append('g')
+        .selectAll("line")
+          .data(ticksValues).enter()
+        .append("line")
+          .attr("x1", 0)
+          .attr("x2", 0)
+          .attr("y1", -chart.internal.radius+8)
+          .attr("y2", -chart.internal.radius)
+          .attr("transform", function (d) {
+              var min = chart.internal.config.gauge_min,
+                  max = chart.internal.config.gauge_max,
+                  ratio = d / (max - min),
+                  rotate = (d - 2) * 45;
+              return "rotate(" + rotate + ")";
+          });
+  setTimeout(function () {
+      chart.load({
+          columns: [['data', 201]]
+      });
+  }, 100);
+  setTimeout(function () {
+      chart.load({
+          columns: [['data', 205]]
+      });
+  }, 1000);
+  setTimeout(function () {
+      chart.load({
+          columns: [['data', 210]]
+      });
+  }, 1500);
+  setTimeout(function () {
+      chart.load({
+          columns: [['data', 215]]
+      });
+  }, 2000);
+  setTimeout(function () {
+      chart.load({
+          columns: [['data', 220]]
+      });
+  }, 2500);
+  setTimeout(function () {
+      chart.load({
+          columns: [['data', 225]]
+      });
+  }, 3000);
+  setTimeout(function () {
+      chart.load({
+          columns: [['data']]
+      });
+  }, 4000);
 }, 3000);
