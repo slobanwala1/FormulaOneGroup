e6ff5ddec958c979850398f2ac7eab087c0c98e3
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2021-02-02 13:09:11 -0600

    Fixing sql

--- FormulaOneDB.sql
+++ FormulaOneDB.sql
@@ -1,149 +1,156 @@
 -- Create all 9 tables for FormulaOneDB
 
 -- Drop tables using CASCADE to remove dependencies as well
-DROP TABLE IF EXISTS circuits CASCADE;
-
-DROP TABLE IF EXISTS races CASCADE;
-
-DROP TABLE IF EXISTS constructors CASCADE;
-
-DROP TABLE IF EXISTS constructor_results CASCADE;
-
-DROP TABLE IF EXISTS constructor_standings CASCADE;
-
-DROP TABLE IF EXISTS drivers CASCADE;
-
-DROP TABLE IF EXISTS results CASCADE;
-
-DROP TABLE IF EXISTS lap_times CASCADE;
 
 DROP TABLE IF EXISTS qualifying CASCADE;
 
+DROP TABLE IF EXISTS lap_times CASCADE;
+
+DROP TABLE IF EXISTS results CASCADE;
+
+DROP TABLE IF EXISTS drivers CASCADE;
+
+DROP TABLE IF EXISTS constructor_standings CASCADE;
+
+DROP TABLE IF EXISTS constructor_results CASCADE;
+
+DROP TABLE IF EXISTS constructors CASCADE;
+
+DROP TABLE IF EXISTS races CASCADE;
+
+DROP TABLE IF EXISTS circuits CASCADE;
+
 
 CREATE TABLE circuits (
-	circuitId INT NOT NULL UNIQUE,
-	circuitRef VARCHAR(50) NOT NULL,
-	circuit_name VARCHAR(50) NOT NULL UNIQUE,
- 	PRIMARY KEY(circuitId)
+	circuitid INT NOT NULL UNIQUE,
+	circuitref VARCHAR(100) NOT NULL,
+	circuit_name VARCHAR(100) NOT NULL,
+	circuit_location VARCHAR(100) NOT NULL,
+	country VARCHAR(100) NOT NULL,
+	lat FLOAT NOT NULL,
+	lng FLOAT NOT NULL,
+	alt INT NOT NULL,
+	url VARCHAR(150) NOT NULL,
+ 	PRIMARY KEY(circuitid)
 );
 
 
 
 CREATE TABLE races (
-	raceId INT NOT NULL UNIQUE,
+	raceid INT NOT NULL UNIQUE,
 	race_year INT NOT NULL,
 	round INT NOT NULL,
-	circuitId INT NOT NULL,
+	circuitid INT NOT NULL,
 	circuit_name VARCHAR(50) NOT NULL,
 	circuit_date DATE NOT NULL,
 	circuit_time TIME,
 	url VARCHAR(150),
- 	PRIMARY KEY(raceId),
-	FOREIGN KEY(circuitId) REFERENCES circuits(circuitId),
-	FOREIGN KEY(circuit_name) REFERENCES circuits(circuit_name)
+	PRIMARY KEY(raceid),
+	FOREIGN KEY(circuitid) REFERENCES circuits(circuitid)
+	--FOREIGN KEY(circuit_name) REFERENCES circuits(circuit_name)
 );
 
 
 CREATE TABLE constructors (
-	constructorId INT NOT NULL,
-	constructorRef VARCHAR(100) NOT NULL,
-	constructor_name VARCHAR(50) NOT NULL,
-	nationality VARCHAR(50) NOT NULL,
-	url VARCHAR(150),
-	PRIMARY KEY(constructorId)
+ 	constructorid INT NOT NULL,
+ 	constructorref VARCHAR(100) NOT NULL,
+ 	constructor_name VARCHAR(50) NOT NULL,
+ 	nationality VARCHAR(50) NOT NULL,
+ 	url VARCHAR(150),
+ 	PRIMARY KEY(constructorid)
 );
 
 
 CREATE TABLE constructor_results (
-	constructorResultsId INT NOT NULL,
-	raceId INT NOT NULL,
-	constructorId INT NOT NULL,
-	points INT NOT NULL,
-	status INT,
-	PRIMARY KEY(constructorResultsId),
-	FOREIGN KEY(raceId) REFERENCES races(raceId),
-	FOREIGN KEY(constructorId) REFERENCES constructors(constructorId)
+ 	constructorresultsid INT NOT NULL,
+ 	raceid INT NOT NULL,
+ 	constructorid INT NOT NULL,
+ 	points INT NOT NULL,
+ 	status INT,
+ 	PRIMARY KEY(constructorresultsid),
+ 	FOREIGN KEY(raceid) REFERENCES races(raceid),
+ 	FOREIGN KEY(constructorid) REFERENCES constructors(constructorid)
 );
 
 
 CREATE TABLE constructor_standings (
-	constructorStandingsId INT NOT NULL,
-	raceId INT NOT NULL,
-	constructorId INT NOT NULL,
-	points INT NOT NULL,
-	constructor_position INT NOT NULL,
-	positionText INT NOT NULL,
-	wins INT NOT NULL,
-	PRIMARY KEY(constructorStandingsId),
-	FOREIGN KEY(raceId) REFERENCES races(raceId),
-	FOREIGN KEY(constructorId) REFERENCES constructors(constructorId)
-);
+ 	constructorstandingsid INT NOT NULL,
+ 	raceid INT NOT NULL,
+ 	constructorid INT NOT NULL,
+ 	points INT NOT NULL,
+ 	constructor_position INT NOT NULL,
+ 	positiontext INT NOT NULL,
+ 	wins INT NOT NULL,
+ 	PRIMARY KEY(constructorstandingsid),
+ 	FOREIGN KEY(raceid) REFERENCES races(raceid),
+ 	FOREIGN KEY(constructorid) REFERENCES constructors(constructorid)
+ );
 
 
 CREATE TABLE drivers (
-	driverId INT NOT NULL,
-	driverRef VARCHAR(50) NOT NULL,
-	driver_number INT,
-	code VARCHAR(5),
-	forename VARCHAR(20) NOT NULL,
-	surname VARCHAR(20) NOT NULL,
-	dob DATE NOT NULL,
-	nationality VARCHAR(20) NOT NULL,
-	url VARCHAR(150),
-	PRIMARY KEY(driverId)
+ 	driverid INT NOT NULL,
+ 	driverref VARCHAR(100) NOT NULL,
+ 	driver_number INT,
+ 	code VARCHAR(10),
+ 	forename VARCHAR(20) NOT NULL,
+ 	surname VARCHAR(20) NOT NULL,
+ 	dob DATE NOT NULL,
+ 	nationality VARCHAR(20) NOT NULL,
+ 	url VARCHAR(150),
+ 	PRIMARY KEY(driverid)
 );
 
 
 CREATE TABLE results (
-	resultId INT NOT NULL,
-	raceId INT NOT NULL,
-	driverId INT NOT NULL,
-	constructorId INT NOT NULL,
-	result_number INT NOT NULL,
-	grid INT NOT NULL,
-	result_position INT,
-	positionText VARCHAR(5),
-	result_points INT NOT NULL,
-	laps INT NOT NULL,
-	result_time VARCHAR(50) NOT NULL,
-	milliseconds INT,
-	fastestLap INT,
-	result_rank INT,
-	fastestLapTime TIME,
-	fastestLapSpeed FLOAT,
-	statusId INT NOT NULL,
-	PRIMARY KEY(resultId),
-	FOREIGN KEY(raceId) REFERENCES races(raceId),
-	FOREIGN KEY(constructorId) REFERENCES constructors(constructorId),
-	FOREIGN KEY(driverId) REFERENCES drivers(driverId)
+ 	resultid INT NOT NULL,
+ 	raceid INT NOT NULL,
+ 	driverid INT NOT NULL,
+ 	constructorid INT NOT NULL,
+ 	result_number INT NOT NULL,
+ 	grid INT NOT NULL,
+ 	result_position INT,
+ 	positiontext VARCHAR(5),
+ 	result_points INT NOT NULL,
+ 	laps INT NOT NULL,
+ 	result_time VARCHAR(50) NOT NULL,
+ 	milliseconds INT,
+ 	fastestLap INT,
+ 	result_rank INT,
+ 	fastestlaptime TIME,
+ 	fastestlapspeed FLOAT,
+ 	statusid INT NOT NULL,
+ 	PRIMARY KEY(resultid),
+ 	FOREIGN KEY(raceid) REFERENCES races(raceid),
+ 	FOREIGN KEY(constructorid) REFERENCES constructors(constructorid),
+ 	FOREIGN KEY(driverid) REFERENCES drivers(driverid)
 );
 
 
 CREATE TABLE lap_times (
-	raceId INT NOT NULL,
-	driverId INT NOT NULL,
-	lap VARCHAR(150) NOT NULL,
+ 	raceid INT NOT NULL,
+	driverid INT NOT NULL,
+ 	lap VARCHAR(150) NOT NULL,
 	lap_time_position INT NOT NULL,
-	lap_time_seconds TIME NOT NULL,
-	milliseconds INT NOT NULL,
-	FOREIGN KEY(raceId) REFERENCES races(raceId),
-	FOREIGN KEY(driverId) REFERENCES drivers(driverId)
+ 	lap_time_seconds TIME NOT NULL,
+ 	milliseconds INT NOT NULL,
+ 	FOREIGN KEY(raceid) REFERENCES races(raceid),
+ 	FOREIGN KEY(driverid) REFERENCES drivers(driverid)
 );
 
 
 CREATE TABLE qualifying (
-	qualifyId INT NOT NULL UNIQUE,
-	raceId INT NOT NULL,
-	driverId INT NOT NULL,
-	constructorId INT NOT NULL,
-	qualify_number INT NOT NULL,
-	qualify_position INT NOT NULL,
-	q1 TIME,
-	q2 TIME,
-	q3 TIME,
- 	PRIMARY KEY(qualifyId),
-	FOREIGN KEY(raceId) REFERENCES races(raceId),
-	FOREIGN KEY(driverId) REFERENCES drivers(driverId),
-	FOREIGN KEY(constructorId) REFERENCES constructors(constructorId)
+ 	qualifyid INT NOT NULL UNIQUE,
+ 	raceid INT NOT NULL,
+ 	driverid INT NOT NULL,
+ 	constructorid INT NOT NULL,
+ 	qualify_number INT NOT NULL,
+ 	qualify_position INT NOT NULL,
+ 	q1 TIME,
+ 	q2 TIME,
+ 	q3 TIME,
+  	PRIMARY KEY(qualifyid),
+	FOREIGN KEY(raceid) REFERENCES races(raceid),
+	FOREIGN KEY(driverid) REFERENCES drivers(driverid),
+ 	FOREIGN KEY(constructorid) REFERENCES constructors(constructorid)
 );
 
