1fb9ca8bc87ecd6c8d9d6bf8737803d8cd1753b2
Shanil Lobanwala <slobanwala@ad.thinkmcs.com>
2021-02-02 23:37:06 -0600

    fixed line graph issue:

--- app/static/graphdocs/linechart.js
+++ app/static/graphdocs/linechart.js
@@ -18,8 +18,8 @@ function displayLineChart(driver1Name, driver2Name, driver1arr, driver2arr) {
       columns: [
       ],
       colors: {
-          // Driver1: '#EB1809',
-          // Driver2: '#141313'
+          Driver1: '#EB1809',
+          Driver2: '#141313'
       }
       //WE WILL HAVE TO ADD A FOR LOOP TO GET THE DRIVERS
   },
@@ -31,7 +31,7 @@ function displayLineChart(driver1Name, driver2Name, driver1arr, driver2arr) {
       show: true,
       position: 'inset',
       inset: {
-        anchor: 'bottom-right'
+        anchor: 'top-right'
       }
   },
   interaction: {
@@ -40,7 +40,7 @@ function displayLineChart(driver1Name, driver2Name, driver1arr, driver2arr) {
   axis: {
     x: {
       show: true,
-      default: [0, 1],
+      default: [0, 40],
       label: {
         text: 'Lap Number',
         position: 'outer-center'
@@ -48,7 +48,7 @@ function displayLineChart(driver1Name, driver2Name, driver1arr, driver2arr) {
     },
     y: {
       show: true,
-      default: [0, 1],
+      default: [0, 10000],
       label: {
         text: 'Lap Time, in milliseconds',
         position: 'outer-center'
@@ -62,6 +62,8 @@ function displayLineChart(driver1Name, driver2Name, driver1arr, driver2arr) {
 }),
 timeout = 1000;
 function addColumn(data, delay){
+  console.log('data!');
+  console.log(data[0]);
 var dataTmp = [data[0], 0];
 setTimeout(function(){
 // linechart.d3.duration(100);
@@ -95,12 +97,13 @@ setTimeout(function(){
 timeout += delay;
 }
 function addLine(value){
+console.log("value: "+value);
 setTimeout(function(){
-// linechart.ygrids.add({value: value});
+linechart.ygrids.add({value: value});
 }, timeout);
-timeout += 100;
+timeout += 10000;
 }
-setTimeout(function(){
+// setTimeout(function(){
 linechart.axis({
 min: {
   x: 120000,
@@ -110,14 +113,14 @@ max: {
   x: 90000,
   y: 80 // change this.
 }
+// });
 });
-}, timeout);
 timeout += 500;
 //here i'll have to add the variable for the drivers
 // addColumn(['DRIVERNAME', 6, 5.95, 6.69, 7.47, 3.53, 0.92, 7.21, 4.02, 3.97, 4.18, 4.2, 6, 5.95, 6.69, 7.47, 3.53, 0.92, 7.21, 4.02, 3.97, 4.18, 4.27, 6, 5.95, 6.69, 7.47, 3.53, 0.92, 7.21, 4.02, 3.97, 4.18, 4.2, 6, 5.95, 6.69, 7.47, 3.53, 0.92, 7.21, 4.02, 3.97, 4.18, 4.27]);
 // addColumn(['DRIVERNAME2', 7.45, 7.31, 8.69, 8.74, 5.7, -6.15, 4.4, 4.67, 3.8, 2.02, 3.04], 2000);
-addColumn(driver1arr, 5000);
-addColumn(driver2arr, 5000);
+addColumn(driver1arr, 10000);
+addColumn(driver2arr, 10000);
 //addColumn(['Country3', 3.82, 3.06, -2.56, 4.43, 2.2, 1.23, 2.53, 2.4, 3.53, 2.62, 2.58], 2000);
 //addColumn(['Country4', 7.58, 8.57, 8.24, 7.54, 4.69, 3.68, 7.62, 6.55, 4.64, 5.02, 4.63], 2000);
 timeout += 1500;
